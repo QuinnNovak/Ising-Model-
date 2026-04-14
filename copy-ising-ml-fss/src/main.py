411641682cf94b2a9e627db2f2fbe54627559e45
Quinn Novak <quinn.novak24601@gmail.com>
2026-04-14 12:36:33 -0700

    Add files via upload

---
+++ copy-ising-ml-fss/src/main.py
@@ -0,0 +1,11 @@
+from .simulations.temperature_scan import run_temperature_scan
+
+
+def main():
+    print("Starting Ising ML + FSS project")
+
+    run_temperature_scan()
+
+
+if __name__ == "__main__":
+    main()
